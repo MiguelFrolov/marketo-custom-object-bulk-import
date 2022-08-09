b2c9c773fe672ed745811888fdbedbfcc22ba802
Mikhail Frolov <mikhail.frolov@hyve.group>
2022-08-10 00:44:26 +0300

    feat: add controller for syncs a list of leads

--- Controllers/MarketoControllers.cs
+++ Controllers/MarketoControllers.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Marketo.Models;
 using Newtonsoft.Json;
-using System.Web;
-using System.Collections.Specialized;
-
+using System.Text;
 
 namespace Marketo.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class tokenController : ControllerBase
+    public class identityController : ControllerBase
     {
         // POST: api/marketo
         [HttpPost] 
@@ -20,6 +18,25 @@ namespace Marketo.Controllers
             var IdentityBody = await IdentityResponse.Content.ReadAsStringAsync();
             ResponseOfIdentity responseOfIdentity = JsonConvert.DeserializeObject<ResponseOfIdentity>(IdentityBody);
             return responseOfIdentity;
+        }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class leadController : ControllerBase
+    {
+        [HttpPost] 
+        public async Task<ActionResult<ResponseOfLead>>  PostLead(LeadQuery leadQuery)
+        {
+            
+            identityController identityController = new identityController();
+            Task<ActionResult<ResponseOfIdentity>> responseOfIdentity = identityController.PostIdentity(leadQuery.identityQuery);
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",  responseOfIdentity.Result.Value.access_token );
+            StringContent leadContent = new StringContent(JsonConvert.SerializeObject(leadQuery.syncLeadRequest), Encoding.UTF8, "application/json");
+            HttpResponseMessage leadResponse = await httpClient.PostAsync(leadQuery.api.baseurl + leadQuery.api.path, leadContent);
+            var LeadBody = await leadResponse.Content.ReadAsStringAsync();
+            ResponseOfLead responseOfLead = JsonConvert.DeserializeObject<ResponseOfLead>(LeadBody);
+            return responseOfLead;
         }
     }
 }
--- Models/MarketoApi.cs
+++ Models/MarketoApi.cs
@@ -16,11 +16,82 @@ namespace Marketo.Models
         public Api api { get; set; }
         public Identity identity { get; set; }
     }
+    public class SyncLeadRequest 
+    {
+        public string action { get; set; }
+        public string lookupField { get; set; }
+        public LeadFileds[] input { get; set; }
+    }
+    public class LeadFileds
+    {
+        public string firstName { get; set; }
+        public string? middleName { get; set; }
+        public string lastName { get; set; }
+        public string? dateOfBirth { get; set; }
+        public string? gender { get; set; }
+        public string? salutation { get; set; }
+        public string email { get; set; }
+        public string? mobilePhone { get; set; }
+        public string? phone { get; set; }
+        public string? fax { get; set; }
+        public string? country { get; set; }
+        public string? postalCode { get; set; }
+        public string? city { get; set; }
+        public string? address { get; set; }
+        public string? company { get; set; }
+        public string? department { get; set; }
+        public string? title { get; set; }
+        public string? industry { get; set; }
+        public string? website { get; set; }
+        public string? numberOfEmployees { get; set; }
+    }
+    public class LeadQuery
+    {
+        public Api api { get; set; }
+        public SyncLeadRequest syncLeadRequest { get; set; }
+        public IdentityQuery identityQuery { get; set; }
+    }
     public class ResponseOfIdentity 
     {
         public string? access_token { get; set; }
         public string? scope { get; set; }
         public int expires_in { get; set; } 
         public string? token_type { get; set; }
+    }
+    public class ResponseOfLead {
+        public Array[] errors { get; set; }
+        public bool? moreResult { get; set; }
+        public string? nextPageToken { get; set; }
+        public string requestId { get; set; }
+        public Lead[] result { get; set; }
+        public bool? success { get; set; }
+        public Warning[] warnings { get; set; }
+    } 
+    public class Error {
+        public string code { get; set; }
+        public string message { get; set; }
+    } 
+    public class Lead {
+        public int? id { get; set; }
+        public ProgramMembership? membership  { get; set; }
+        public Reason? reason { get; set; }
+        public string? status { get; set; }
+        }
+    public class Warning {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+    public class ProgramMembership {
+        public bool? acquiredBy { get; set; }
+        public bool? isExhausted { get; set; }
+        public string stringmembershipDate { get; set; }
+        public string? nurtureCadence { get; set; }
+        public string progressionStatus { get; set; }
+        public bool? reachedSuccess { get; set; }
+        public string? stream { get; set; }
+    }
+    public class Reason {
+        public string code { get; set; }
+        public string message { get; set; }
     }
 }
